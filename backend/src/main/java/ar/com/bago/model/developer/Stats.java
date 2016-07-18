9ebec62900a2b9c7d821cd334b24e2531d1b5981
Diego Saez <diego.saez@centauri.co>
2016-07-18 14:50:16 -0300

    Agregando unit test para el repositorio de dbutils

--- backend/src/main/java/ar/com/bago/model/developer/Stats.java
+++ backend/src/main/java/ar/com/bago/model/developer/Stats.java
@@ -1,5 +1,7 @@
 package ar.com.bago.model.developer;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class Stats {
@@ -27,6 +29,22 @@ public class Stats {
 
     public void setSeniorityValue(int seniorityValue) {
         this.seniorityValue = seniorityValue;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj == this) {
+            return true;
+        }
+        if (obj.getClass() != getClass()) {
+            return false;
+        }
+        Stats stats = (Stats) obj;
+        return new EqualsBuilder().append(seniorityValue, stats.seniorityValue).append(developers, stats.developers)
+                .isEquals();
     }
 
 }
